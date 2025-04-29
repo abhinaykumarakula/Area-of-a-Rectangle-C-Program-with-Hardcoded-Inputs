28c954a1c85e3a20c80760da460fd0e0f0e8ecd9
ABHINAY <abhinaykumarakula5@gmail.com>
2025-04-29 23:22:52 +0530

    Add files via upload

---
+++ 01_program_a.c
@@ -0,0 +1,17 @@
+// OM NAMO VENKATESHAYA
+// AUTHOR   : ABHINAY 
+/* PROGRAM  : AREA OF RECTANGLE BY USING
+              HARD CODED INPUTS */
+
+#include <stdio.h> 
+
+int main(){ 
+    int length,breadth,area;
+    length = 6;
+    breadth = 3;
+    //area of rectangle = length * breadth
+    area = length * breadth;
+
+    printf("AREA OF THE RECTANGLE : %d\n",area);
+    return 0;
+}
